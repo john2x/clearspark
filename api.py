648e702177ee567aab09a5ee5261c455483263ff
rsimba <robin.singh.1991@gmail.com>
2014-12-04 16:42:20 -0500

    lol

--- api.py
+++ api.py
@@ -59,7 +59,8 @@ def app_company_info():
 def app_company_info_webhook():
     company = check_if_company_exists_in_db(request.args)
     q.enqueue(Companies()._get_info_webhook, request.args['company_name'], 
-                                             request.args['objectId'])
+                                             request.args['objectId'],
+                                             timeout=3600)
     return company if company else {'': 'Your query has been queued.'}
 
 '''  **************************
