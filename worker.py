aae88ccf217b1d0ed051cd3396da3902c1b0f985
rsimba <robin.singh.1991@gmail.com>
2014-12-29 19:56:10 -0500

    lol

--- worker.py
+++ worker.py
@@ -6,12 +6,16 @@ import urlparse
 listen = ['high', 'default', 'low']
 '''
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-#redis_url = os.getenv('REDISCLOUD_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 '''
 
+'''
 url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
 conn = redis.Redis(host=url.hostname, port=url.port, password=url.password)
+'''
+
+redis_url = os.getenv('REDISCLOUD_URL', 'redis://localhost:6379')
+conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
   with Connection(conn):
