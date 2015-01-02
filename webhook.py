13cdef41a76f604c41d72ba78970d85e14cf4760
rsimba <robin.singh.1991@gmail.com>
2015-01-02 14:25:09 -0500

    api key test

--- webhook.py
+++ webhook.py
@@ -3,7 +3,7 @@ import requests
 
 class Webhook:
     def _post(self, api_key, data):
-        qry = {'where':json.dumps('api_key':api_key, 'data':data)}
+        qry = {'where':json.dumps({'api_key':api_key, 'data':data})}
         urls = Parse().get('Webhook', qry).json()['results']
         for url in urls:
             headers = {'content-type': 'application/json'}
