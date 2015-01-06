9dc93bee781e1c3d78e5e0105e8b292b9f0bbd01
rsimba <robin.singh.1991@gmail.com>
2015-01-06 13:45:08 -0500

    lol

--- mining_job.py
+++ mining_job.py
@@ -26,9 +26,9 @@ class MiningJob:
             prospect['pos'] = row['title']
             prospect['city'] = row['locale']
             prospect['lists'] = [Parse()._pointer('ProspectList', list_id)]
-            del prospect['objectId']
-            del prospect['createdAt']
-            del prospect['updatedAt']
+            #del prospect['objectId']
+            #del prospect['createdAt']
+            #del prospect['updatedAt']
             r = Prospecter().create('Prospect', company)
             print "prospect_create_result", r.json()
             #if index > limit: break
