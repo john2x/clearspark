fa820ce8860f4c0f6c04d935d57748254f2cdbdc
rsimba <robin.singh.1991@gmail.com>
2015-02-09 11:06:46 -0500

    lol

--- sources.py
+++ sources.py
@@ -180,7 +180,9 @@ class Sources:
         browser.find_by_css('#login_btn').first.click()
         browser.find_by_css('#homepageSBS').first.fill(company_name)
         browser.find_by_css('.homepage-search-icon').first.click()
-        print browser.html
+        for i in browser.find_by_css('.company_name'):
+          print i.text
+
         if len(browser.find_by_css('.companyName')):
             time.sleep(1)
             browser.find_by_css('.companyName').first.click()
