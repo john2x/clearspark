49b1c938d7c7880ef842264f2e635f37e310aad1
rsimba <robin.singh.1991@gmail.com>
2015-03-17 21:49:17 -0400

    lol

---
+++ setting.py
@@ -0,0 +1,3 @@
+# If you're using Sentry to collect your runtime exceptions, you can use this
+# to configure RQ for it in a single step
+SENTRY_DSN = 'https://c454ce266862478a8edb143d99669f18:086b5a8c99074cab8f71926f3e8235cb@app.getsentry.com/39949'
